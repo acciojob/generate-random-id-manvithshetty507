3981280872d2c6dcb1adde8bb0da270dd8751cd7
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-12 14:13:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function makeid(l) {
+function makeid(len) {
   // write your code here
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	let ans = '';
+	for(let i=0;i<len;i++) {
+		ans += characters[Math.floor(Math.random()*characters.length)]
+	}
+	return ans
 }
 
 // Do not change the code below.
